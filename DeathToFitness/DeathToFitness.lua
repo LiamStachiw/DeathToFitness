44ff89a58e4d4d2ae0c01ac4eff7cffa7872470b
Liam Stachiw <liam.stachiw@gmail.com>
2020-12-30 18:06:18 -0500

    Basic functionality completed
    It can now tell if the player died in a boss encounter and calculate the amount of exercise correctly based on the number of bosses in the encounter.

--- DeathToFitness/DeathToFitness.lua
+++ DeathToFitness/DeathToFitness.lua
@@ -4,7 +4,34 @@ Exercise_EventFrame:RegisterEvent("PLAYER_DEAD")
 
 Exercise_EventFrame:SetScript("OnEvent",
     function(self, event, ...)
-        local currentHealth = UintHealth("boss1")
-        local maxHealth = UnitHealthMax("boss1")
-        print("Amount of exercise: " .. math.floor(((currentHealth / maxHealth) * 100) / 2))
+        -- Initalize current and max boss health pools
+        local currentHealth = 0
+        local maxHealth = 0
+        
+        -- If there is at least one boss (This will prevent anything from printing when dying to trash)
+        if UnitHealth("boss1") ~= 0 then
+            -- If there are 4 bosses
+            if UnitHealth("boss4") ~= 0 then
+                -- Sum the current and max health of all bosses
+                currentHealth = UnitHealth("boss1") + UnitHealth("boss2") + UnitHealth("boss3") + UnitHealth("boss4")
+                maxHealth = UnitHealthMax("boss1") +  UnitHealthMax("boss2") + UnitHealthMax("boss3") + UnitHealthMax("boss4")
+            -- If there are 3 bosses
+            elseif UnitHealth("boss3") ~= 0 then
+                -- Sum the current and max health of all bosses
+                currentHealth = UnitHealth("boss1") + UnitHealth("boss2") + UnitHealth("boss3")
+                maxHealth = UnitHealthMax("boss1") +  UnitHealthMax("boss2") + UnitHealthMax("boss3")
+            -- If there are 2 bosses
+            elseif UnitHealth("boss2") ~= 0 then
+                -- Sum the current and max health of all bosses
+                currentHealth = UnitHealth("boss1") + UnitHealth("boss2")
+                maxHealth = UnitHealthMax("boss1") +  UnitHealthMax("boss2")
+            -- If there is only 1 boss
+            else
+                -- Get the boss' current and max health
+                currentHealth = UnitHealth("boss1")
+                maxHealth = UnitHealthMax("boss1")
+            end
+            -- Calculate and print the amount of exercise to do.
+            print("Amount of exercise: " .. math.floor(((currentHealth / maxHealth) * 100) / 2))
+		end
     end)
